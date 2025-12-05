6cf22198cbd8e1aa8539ceb55e844441e433bab1
Luke Woods <159604359+lukewoodstech@users.noreply.github.com>
2025-12-05 10:46:03 -0700

    fix: add noStore() and revalidate=0 to prevent all caching

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,10 +1,14 @@
 import { getAppointments, getPets } from "../actions/appointments";
 import { getActiveServices } from "../actions/services";
 import { SchedulePageContent } from "./client-components";
+import { unstable_noStore as noStore } from 'next/cache';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default async function SchedulePage() {
+  noStore();
+
   const appointments = await getAppointments();
   const pets = await getPets();
   const services = await getActiveServices();
