38cdbd4e6953ff1cebc36a5a008d2e556839ea41
Luke Woods <159604359+lukewoodstech@users.noreply.github.com>
2025-12-05 10:50:16 -0700

    fix: remove unstable_noStore() call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import { getAppointments, getPets } from "./actions/appointments";
 import { getActiveServices } from "./actions/services";
 import { CalendarPageContent } from "./calendar-components";
-import { unstable_noStore as noStore } from 'next/cache';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default async function Home() {
-  noStore();
-
   const appointments = await getAppointments();
   const pets = await getPets();
   const services = await getActiveServices();
--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,14 +1,11 @@
 import { getAppointments, getPets } from "../actions/appointments";
 import { getActiveServices } from "../actions/services";
 import { SchedulePageContent } from "./client-components";
-import { unstable_noStore as noStore } from 'next/cache';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default async function SchedulePage() {
-  noStore();
-
   const appointments = await getAppointments();
   const pets = await getPets();
   const services = await getActiveServices();
